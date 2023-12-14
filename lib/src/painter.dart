3ab4e97a776467cba04b9e5c0f850b7130fd05ec
ulmas <olmasbek.rakhmatullaev@eriell.com>
2023-12-14 18:49:17 +0500

    and another one

--- lib/src/painter.dart
+++ lib/src/painter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../components/canvas/touchy_canvas.dart';
 import '../countries_world_map.dart';
+import 'package:path_drawing/path_drawing.dart';
 
 /// This painter will paint a world map with all///
 /// Giving countries a different color based on a data set can help visualize data.
@@ -50,13 +51,16 @@ class SimpleMapPainter extends CustomPainter {
           countryPathList[i].uniqueID == 'UZ-ER') {
         print('prolong');
         Paint paint = Paint()..color = Colors.black;
-        c.drawCircle(
-            Offset(
-              s.width * double.parse(paths.first.split(',').first),
-              s.height * double.parse(paths.first.split(',').last),
-            ),
-            1,
-            paint);
+        final Path complexPathToDraw = parseSvgPathData(
+            "M8.70711 0.292893C8.31658 -0.097631 7.68342 -0.097631 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 2V1L7 1V2L9 2Z");
+        canvas.drawPath(complexPathToDraw, Paint());
+        // c.drawCircle(
+        //     Offset(
+        //       s.width * double.parse(paths.first.split(',').first),
+        //       s.height * double.parse(paths.first.split(',').last),
+        //     ),
+        //     1,
+        //     paint);
       } else {
         for (int j = 0; j < paths.length; j++) {
           String instruction = paths[j];
